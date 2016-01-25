f408aa8345fc0f7bd8e86f6ea856ebc6809bae26
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2016-01-25 09:25:46 +0000

    added tests

--- test/ramp_encoding_SUITE.erl
+++ test/ramp_encoding_SUITE.erl
@@ -8,6 +8,7 @@ all() ->
 groups() ->
     [{main, [parallel], common:tests(?MODULE)}].
 
+%%  JSON
 
 hello_json_test(_) ->
     M = ramp_message:hello(<<"realm1">>, #{roles => #{
@@ -171,3 +172,170 @@ yield_json_2_test(_) ->
 yield_json_3_test(_) ->
     M = ramp_message:yield(1, #{}, [], #{}),
     {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, json), text, json).
+
+
+%% ERL
+
+
+hello_erl_test(_) ->
+    M = ramp_message:hello(<<"realm1">>, #{roles => #{
+        caller => #{}
+    }}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+welcome_erl_test(_) ->
+    M = ramp_message:welcome(1, #{roles => #{
+        dealer => #{},
+        broker => #{}
+    }}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+abort_erl_test(_) ->
+    M = ramp_message:abort(#{message => <<"foo">>}, <<"wamp.error.foo">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+challenge_erl_test(_) ->
+    M = ramp_message:challenge(<<"foo">>, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+authenticate_erl_test(_) ->
+    M = ramp_message:authenticate(<<"foo">>, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+goodbye_erl_test(_) ->
+    M = ramp_message:goodbye(
+        #{message => <<"The host is shutting down now.">>},
+        <<"wamp.error.system_shutdown">>
+    ),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+heartbeat_erl_test(_) ->
+    ok.
+
+error_erl_test(_) ->
+    M = ramp_message:error(0, 1, #{}, <<"wamp.error.foo">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+error_erl_2_test(_) ->
+    M = ramp_message:error(0, 1, #{}, <<"wamp.error.foo">>, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+error_erl_3_test(_) ->
+    M = ramp_message:error(0, 1, #{}, <<"wamp.error.foo">>, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+publish_erl_test(_) ->
+    M = ramp_message:publish(1, #{}, <<"com.williamhill.topic1">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+publish_erl_2_test(_) ->
+    M = ramp_message:publish(1, #{}, <<"com.williamhill.topic1">>, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+publish_erl_3_test(_) ->
+    M = ramp_message:publish(1, #{}, <<"com.williamhill.topic1">>, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+published_erl_test(_) ->
+    M = ramp_message:published(1, 2),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+subscribe_erl_test(_) ->
+    M = ramp_message:subscribe(1, #{}, <<"com.williamhill.topic1">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+subscribed_erl_test(_) ->
+    M = ramp_message:subscribed(1, 3),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+unsubscribe_erl_test(_) ->
+    M = ramp_message:unsubscribe(1, 3),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+unsubscribed_erl_test(_) ->
+    M = ramp_message:unsubscribed(1),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+event_erl_test(_) ->
+    M = ramp_message:event(3, 2, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+event_erl_2_test(_) ->
+    M = ramp_message:event(3, 2, #{}, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+event_erl_3_test(_) ->
+    M = ramp_message:event(3, 2, #{}, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+call_erl_test(_) ->
+    M = ramp_message:call(1, #{}, <<"com.williamhill.myprocedure1">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+call_erl_2_test(_) ->
+    M = ramp_message:call(1, #{}, <<"com.williamhill.myprocedure1">>, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+call_erl_3_test(_) ->
+    M = ramp_message:call(1, #{}, <<"com.williamhill.myprocedure1">>, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+cancel_erl_test(_) ->
+    M = ramp_message:cancel(1, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+result_erl_test(_) ->
+    M = ramp_message:result(1, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+result_erl_2_test(_) ->
+    M = ramp_message:result(1, #{}, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+result_erl_3_test(_) ->
+    M = ramp_message:result(1, #{}, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+register_erl_test(_) ->
+    M = ramp_message:register(1, #{}, <<"com.williamhill.myprocedure1">>),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+registered_erl_2_test(_) ->
+    M = ramp_message:registered(1, 4),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+unregister_erl_3_test(_) ->
+    M = ramp_message:unregister(1, 4),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+unregistered_erl_test(_) ->
+    M = ramp_message:unregistered(1),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+invocation_erl_test(_) ->
+    M = ramp_message:invocation(1, 4, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+invocation_erl_2_test(_) ->
+    M = ramp_message:invocation(1, 4, #{}, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+invocation_erl_3_test(_) ->
+    M = ramp_message:invocation(1, 4, #{}, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+interrupt_erl_test(_) ->
+    M = ramp_message:interrupt(1, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+yield_erl_test(_) ->
+    M = ramp_message:yield(1, #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+yield_erl_2_test(_) ->
+    M = ramp_message:yield(1, #{}, []),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
+
+yield_erl_3_test(_) ->
+    M = ramp_message:yield(1, #{}, [], #{}),
+    {[M], <<>>} = ramp_encoding:decode(ramp_encoding:encode(M, erl), text, erl).
