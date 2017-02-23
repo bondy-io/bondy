8180a8fd3c96db4b5d34df77c23bf62fb570564d
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-02-23 16:25:59 +0000

    minor changes

---
+++ apps/juno/test/juno_SUITE.erl
@@ -0,0 +1,17 @@
+-module(juno_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-compile(export_all).
+
+all() ->
+    common:all().
+
+groups() ->
+    [{main, [parallel], common:tests(?MODULE)}].
+
+%%  JSON
+
+hello_json_test(_) ->
+    M = wamp_message:hello(<<"realm1">>, #{roles => #{
+        caller => #{}
+    }}),
+    {[M], <<>>} = wamp_encoding:decode(wamp_encoding:encode(M, json), text, json).
