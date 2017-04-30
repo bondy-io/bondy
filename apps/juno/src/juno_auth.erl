36f00e0ed1bdcbad4207a7a407c90c2683f9b108
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-04-30 19:02:52 +0100

    added comments

--- apps/juno/src/juno_auth.erl
+++ apps/juno/src/juno_auth.erl
@@ -15,6 +15,36 @@
                             | {user_not_found, binary()}.
 
 
+% -record(oauth2_credentials, {
+%     id                  ::  binary(),
+%     consumer_id         ::  binary(),
+%     name                ::  binary(),
+%     client_id           ::  binary(),
+%     client_secret       ::  binary(),
+%     redirect_uri        ::  binary(),
+%     created_at          ::  binary()
+% }).
+
+% -record(oauth2_auth_codes, {
+%     id                      ::  binary(),
+%     api_id                  ::  binary(),
+%     code                    ::  binary(),
+%     authenticated_userid    ::  binary(),
+%     scope                   ::  binary(),
+%     created_at              ::  binary()
+% }).
+
+% -record(jwt, {
+%     id                      ::  binary(),
+%     created_at              ::  binary(),
+%     consumer_id             ::  binary(),
+%     key                     ::  binary(),
+%     secret                  ::  binary(),
+%     rsa_public_key          ::  binary(),
+%     algorithm               ::  binary() %% HS256 RS256 ES256
+% }).
+
+
 %% =============================================================================
 %% API
 %% =============================================================================
