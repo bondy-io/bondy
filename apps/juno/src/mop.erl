d32dca1b1c3131b5699cc72a8c98f2fa400bf96f
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-05-19 23:51:46 +0100

    fixed bug in get/2

--- apps/juno/src/mop.erl
+++ apps/juno/src/mop.erl
@@ -65,13 +65,43 @@ eval(Val, Ctxt) ->
 
 %% -----------------------------------------------------------------------------
 %% @doc
+%% Evaluates **Term** ( `any()`) using the context **Ctxt** (`map()`).
+%% Returns **Term** in case the term is not a binary and does not contain a 
+%% mop expression. Otherwise, it tries to resolve the expression using the
+%% **Ctxt**.
+%%
+%% The following are valid mop expressions and their meanings:
+%% - `<<"{{foo}}">>` - resolve the value for key `<<"foo">>` in the context. 
+%% It fails if the context does not have a key named `<<"foo">>`.
+%% - `<<"\"{{foo}}\">>` - return a string by resolving the value for key 
+%% `<<"foo">>` in the context. It fails if the context does not have a key 
+%% named `<<"foo">>`.
+%% - `<<"{{foo.bar}}">>` - resolve the value for key path in the context.
+%% It fails if the context does not have a key named `<<"foo">>` which has a value that is either a `function()` or a `map()` with a key named `<<"bar">>`.
+%% - `<<"{{foo |> integer}}">>` - resolves the value for key <<"foo">> in the 
+%% context and converts the result to an integer. If the result was a 
+%% `function()`, it returns a function composition. 
+%% It fails if the context does not have a key named `<<"foo">>`
+%%
+%% Examples:
+%% ```
+%% > mop:eval(foo, #{<<"foo">> => bar}).
+%% > foo
+%% > mop:eval(<<"foo">>, #{<<"foo">> => bar}).
+%% > <<"foo">>
+%% > mop:eval(<<"{{foo}}">>, #{<<"foo">> => bar}).
+%% > bar
+%% > mop:eval(<<"{{foo.bar}}">>, #{<<"foo">> => #{<<"bar">> => foobar}}).
+%% > foobar
+%% ```
 %% @end
 %% -----------------------------------------------------------------------------
--spec eval(any(), Ctxt :: map(), Opts :: map()) -> any() | no_return().
+-spec eval(Term :: any(), Ctxt :: map(), Opts :: map()) -> any() | no_return().
+
 eval(<<>>, _, _) ->
     <<>>;
 
-eval(Val, Ctxt, Opts) when is_binary(Val), is_map(Opts) ->
+eval(Val, Ctxt, Opts) when is_binary(Val), is_map(Ctxt), is_map(Opts) ->
     do_eval(Val, #state{
         context = Ctxt, 
         %% TODO Opts not being used 
@@ -92,7 +122,10 @@ eval(Val, _, _) ->
 
 %% @private
 -spec do_eval(binary(), state()) -> any() | no_return().
+
 do_eval(<<>>, #state{is_ground = true} = St) ->
+    %% The end of a string (quoted expression)
+    %% We reverse the list of terms and turn them into iolist()
     Fun = fun
         (X, Acc) -> 
             [term_to_iolist(X)|Acc] 
@@ -100,7 +133,10 @@ do_eval(<<>>, #state{is_ground = true} = St) ->
     iolist_to_binary(
         lists:foldl(Fun, [], [?DOUBLE_QUOTES | St#state.acc]));
 
-do_eval(<<>>, #state{is_ground = false} =St) ->
+do_eval(<<>>, #state{is_ground = false} = St) ->
+    %% The end of a string (quoted expression)
+    %% We have functions so we wrap the iolist in a function receiving
+    %% a future context as an argument
     fun(Ctxt) ->
         Eval = fun
             (F, Acc) when is_function(F, 1) ->
@@ -113,11 +149,14 @@ do_eval(<<>>, #state{is_ground = false} =St) ->
     end;
 
 do_eval(Bin0, #state{acc = []} = St0) ->
+    %% We start processing a binary
     Bin1 = trim(Bin0),
     case binary:match(Bin1, ?START) of
         nomatch ->
+            %% Not a mop expression, so we return and finish
             Bin1;         
         {Pos, 2}  ->
+            %% Maybe a mop expression as we found the left moustache
             St1 = St0#state{is_open = true},
             Len = byte_size(Bin1),
             case binary:matches(Bin1, ?DOUBLE_QUOTES) of
@@ -148,6 +187,7 @@ do_eval(Bin, #state{is_open = true} = St0) ->
             St1 = St0#state{is_open = false},
             do_eval(Rest, acc(St1, parse_expr(Expr, St1)));
         _ ->
+            %% We did not find a matching closing mustaches
             error(badarg)
     end;
 
@@ -156,11 +196,17 @@ do_eval(Bin, #state{is_open = false} = St) ->
         [Pre, Rest]->
             do_eval(Rest, acc(St#state{is_open = true}, Pre));
         [Bin] ->
+            %% No more mustaches so we finish
             do_eval(<<>>, acc(St, Bin))
     end.
     
 
 %% @private
+%% -----------------------------------------------------------------------------
+%% @doc
+%% Accummulates the evaluation in the case of strings.
+%% @end
+%% -----------------------------------------------------------------------------
 acc(#state{acc = []} = St, Val) ->
     acc(St#state{acc= [?DOUBLE_QUOTES]}, Val);
 
@@ -186,8 +232,11 @@ acc(#state{acc = Acc} = St, Val) ->
 %% -----------------------------------------------------------------------------
 parse_expr(Bin, #state{context = Ctxt}) ->
     case binary:split(Bin, ?PIPE_OP, [global]) of
-        [Val | Pipes] ->
-            apply_ops([trim(P) || P <- Pipes], get_value(trim(Val), Ctxt));
+        [Val] ->
+            get_value(trim(Val), Ctxt);
+        [Val | Ops] ->
+            %% We evaluate the value and apply the ops in the pipe
+            apply_ops([trim(P) || P <- Ops], get_value(trim(Val), Ctxt));
         _ ->
             error(badarg)
     end.
@@ -204,6 +253,10 @@ get_value(Key, Ctxt) ->
 
 
 %% @private
+
+get([], Ctxt) ->
+    Ctxt;
+
 get(Key, F) when is_function(F) ->
     fun(X) ->
         try 
@@ -216,10 +269,7 @@ get(Key, F) when is_function(F) ->
     end;
 
 get([H|T], Ctxt) when is_map(Ctxt) ->
-    get(T, maps:get(H, Ctxt));
-
-get([], Ctxt) ->
-    Ctxt.
+    get(T, maps:get(H, Ctxt)).
 
 
 %% @private
