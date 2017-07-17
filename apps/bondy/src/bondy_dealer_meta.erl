eaac0543ce639e71358d489a1f7e95d58a82794e
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-07-17 21:30:23 +0100

    cleaner implementation of meta procedures; harmonised around {error, not_found}

--- apps/bondy/src/bondy_dealer_meta.erl
+++ apps/bondy/src/bondy_dealer_meta.erl
@@ -79,251 +79,134 @@ handle_call(
     R = wamp_message:result(ReqId, #{}, [], Res),
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
+%% =============================================================================
+%% BONDY META PROCEDURES
 handle_call(#call{
         procedure_uri = <<"com.leapsight.bondy.realms.enable_security">>} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    R = case bondy_context:realm_uri(Ctxt) of
-        ?BONDY_REALM_URI ->        
-            case M#call.arguments of
-                [Uri] ->
-                    maybe_error(
-                        bondy_realm:enable_security(bondy_realm:get(Uri)),
-                        ReqId);
-                _ ->
-                    wamp_message:error(
-                        ?CALL,
-                        ReqId,
-                        #{},
-                        ?WAMP_ERROR_INVALID_ARGUMENT)
-            end;
-        _ ->
-            unauthorized(ReqId, Ctxt)
+    R = case validate_admin_call_args(M, Ctxt, 1) of
+        {ok, [Uri]} ->
+            maybe_error(bondy_realm:enable_security(bondy_realm:get(Uri)), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
+
 handle_call(#call{
         procedure_uri = <<"com.leapsight.bondy.realms.disable_security">>} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    R = case bondy_context:realm_uri(Ctxt) of
-        ?BONDY_REALM_URI ->        
-            case M#call.arguments of
-                [Uri] ->
-                    maybe_error(
-                        bondy_realm:disable_security(bondy_realm:get(Uri)),
-                        ReqId);
-                _ ->
-                    wamp_message:error(
-                        ?CALL,
-                        ReqId,
-                        #{},
-                        ?WAMP_ERROR_INVALID_ARGUMENT)
-            end;
-        _ ->
-            unauthorized(ReqId, Ctxt)
+    R = case validate_admin_call_args(M, Ctxt, 1) of
+        {ok, [Uri]} ->
+            maybe_error(bondy_realm:disable_security(bondy_realm:get(Uri)), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_GATEWAY_LOAD_API_SPEC} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    R = case bondy_context:realm_uri(Ctxt) of
-        ?BONDY_REALM_URI ->        
-            case M#call.arguments of
-                [Spec] ->
-                    maybe_error(
-                        bondy_api_gateway:load(Spec), ReqId);
-                _ ->
-                    wamp_message:error(
-                        ?CALL,
-                        ReqId,
-                        #{},
-                        ?WAMP_ERROR_INVALID_ARGUMENT)
-            end;
-        _ ->
-            unauthorized(ReqId, Ctxt)
+    R = case validate_admin_call_args(M, Ctxt, 1) of
+        {ok, [Spec]} ->
+            maybe_error(bondy_api_gateway:load(Spec), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_GATEWAY_CLIENT_ADD} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    MyRealmUri = bondy_context:realm_uri(Ctxt),
-    R = case {MyRealmUri, M#call.arguments} of
-        {MyRealmUri, [Info]} ->
-            maybe_error(
-                bondy_api_gateway:add_client(MyRealmUri, Info),
-                ReqId
-            );
-        {MyRealmUri, [Uri, Info]} 
-        when MyRealmUri =:= Uri orelse MyRealmUri =:= ?BONDY_REALM_URI ->
-            maybe_error(
-                bondy_api_gateway:add_client(Uri, Info),
-                ReqId
-            );
-        {_, [_, _]} ->
-            unauthorized(ReqId, Ctxt);
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Info]} ->
+            maybe_error(bondy_api_gateway:add_client(Uri, Info), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 
 handle_call(#call{procedure_uri = <<"com.leapsight.bondy.api_gateway.add_resource_owner">>} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    MyRealmUri = bondy_context:realm_uri(Ctxt),
-    R = case {MyRealmUri, M#call.arguments} of
-        {MyRealmUri, [Info]} ->
-            maybe_error(
-                bondy_api_gateway:add_resource_owner(MyRealmUri, Info),
-                ReqId
-            );
-        {MyRealmUri, [Uri, Info]} 
-        when MyRealmUri =:= Uri orelse MyRealmUri =:= ?BONDY_REALM_URI ->
-            maybe_error(
-                bondy_api_gateway:add_resource_owner(Uri, Info),
-                ReqId
-            );
-        {_, [_, _]} ->
-            unauthorized(ReqId, Ctxt);
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Info]} ->
+            maybe_error(bondy_api_gateway:add_resource_owner(Uri, Info), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 
 handle_call(#call{procedure_uri = <<"com.leapsight.bondy.api_gateway.update_resource_owner">>} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    MyRealmUri = bondy_context:realm_uri(Ctxt),
-    R = case {MyRealmUri, M#call.arguments} of
-        {MyRealmUri, [Username, Info]} ->
+    R = case validate_call_args(M, Ctxt, 3) of
+        {ok, [Uri, Username, Info]} ->
             maybe_error(
-                bondy_api_gateway:update_resource_owner(MyRealmUri, Username, Info),
-                ReqId
+                bondy_api_gateway:update_resource_owner(Uri, Username, Info), 
+                M
             );
-        {MyRealmUri, [Uri, Username, Info]} 
-        when MyRealmUri =:= Uri orelse MyRealmUri =:= ?BONDY_REALM_URI ->
-            maybe_error(
-                bondy_api_gateway:update_resource_owner(Uri, Username, Info),
-                ReqId
-            );
-        {_, [_, _]} ->
-            unauthorized(ReqId, Ctxt);
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = <<"com.leapsight.bondy.api_gateway.remove_resource_owner">>} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    MyRealmUri = bondy_context:realm_uri(Ctxt),
-    R = case {MyRealmUri, M#call.arguments} of
-        {MyRealmUri, [Username]} ->
-            maybe_error(
-                bondy_api_gateway:remove_resource_owner(MyRealmUri, Username),
-                ReqId
-            );
-        {MyRealmUri, [Uri, Username]} 
-        when MyRealmUri =:= Uri orelse MyRealmUri =:= ?BONDY_REALM_URI ->
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Username]} ->
             maybe_error(
                 bondy_api_gateway:remove_resource_owner(Uri, Username),
-                ReqId
+                M
             );
-        {_, [_, _]} ->
-            unauthorized(ReqId, Ctxt);
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_USER_ADD} = M, Ctxt) ->
-    %% @TODO
-    ReqId = M#call.request_id,
-    R = case M#call.arguments of
-        [User] ->
-            Uri = maps:get(realm_uri, Ctxt),
-            case bondy_security_user:add(Uri, User) of
-                ok ->
-                    wamp_message:result(ReqId, #{}, [], #{});
-                {error, Reason} ->
-                    #{<<"code">> := Code} = Map = bondy_error:error_map(Reason),
-                    wamp_message:error(
-                        ?CALL,
-                        ReqId,
-                        Map,
-                        bondy_error:error_uri(Code))
-            end;
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Info]} ->
+            maybe_error(bondy_security_user:add(Uri, Info), M);
+        {error, WampError} ->
+            WampError     
+    end,
+    bondy:send(bondy_context:peer_id(Ctxt), R);
+
+handle_call(#call{procedure_uri = ?BONDY_USER_UPDATE} = M, Ctxt) ->
+    R = case validate_call_args(M, Ctxt, 3) of
+        {ok, [Uri, Username, Info]} ->
+            maybe_error(bondy_security_user:update(Uri, Username, Info), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_USER_DELETE} = M, Ctxt) ->
-    %% @TODO
-    ReqId = M#call.request_id,
-    Res = #{},
-    R = wamp_message:result(ReqId, #{}, [], Res),
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Username]} ->
+            maybe_error(bondy_security_user:remove(Uri, Username), M);
+        {error, WampError} ->
+            WampError     
+    end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_USER_LIST} = M, Ctxt) ->
-    ReqId = M#call.request_id,
-    R = case M#call.arguments of
-        [Uri] ->
-            case bondy_security_user:list(Uri) of
-                L when is_list(L) ->
-                    wamp_message:result(ReqId, #{}, [L], #{});
-                {error, Reason} ->
-                    #{<<"code">> := Code} = Map = bondy_error:error_map(Reason),
-                    wamp_message:error(
-                        ?CALL,
-                        ReqId,
-                        Map,
-                        bondy_error:error_uri(Code))
-            end;
-        _ ->
-            wamp_message:error(
-                ?CALL,
-                ReqId,
-                #{},
-                ?WAMP_ERROR_INVALID_ARGUMENT)
+    R = case validate_call_args(M, Ctxt, 1) of
+        {ok, [Uri]} ->
+            maybe_error(bondy_security_user:list(Uri), M);
+        {error, WampError} ->
+            WampError     
     end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_USER_LOOKUP} = M, Ctxt) ->
-    %% @TODO
-    ReqId = M#call.request_id,
-    Res = #{},
-    R = wamp_message:result(ReqId, #{}, [], Res),
-    bondy:send(bondy_context:peer_id(Ctxt), R);
-
-handle_call(#call{procedure_uri = ?BONDY_USER_UPDATE} = M, Ctxt) ->
-    %% @TODO
-    ReqId = M#call.request_id,
-    Res = #{},
-    R = wamp_message:result(ReqId, #{}, [], Res),
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Username]} ->
+            maybe_error(bondy_security_user:lookup(Uri, Username), M);
+        {error, WampError} ->
+            WampError     
+    end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_GROUP_ADD} = M, Ctxt) ->
     %% @TODO
-    ReqId = M#call.request_id,
-    Res = #{},
-    R = wamp_message:result(ReqId, #{}, [], Res),
+    R = case validate_call_args(M, Ctxt, 2) of
+        {ok, [Uri, Info]} ->
+            maybe_error(bondy_security_group:add(Uri, Info), M);
+        {error, WampError} ->
+            WampError     
+    end,
     bondy:send(bondy_context:peer_id(Ctxt), R);
 
 handle_call(#call{procedure_uri = ?BONDY_GROUP_DELETE} = M, Ctxt) ->
@@ -384,32 +267,99 @@ handle_call(#call{procedure_uri = ?BONDY_SOURCE_LOOKUP} = M, Ctxt) ->
 
 
 %% @private
-maybe_error(ok, ReqId) ->
-    wamp_message:result(ReqId, #{}, [], #{});
+maybe_error(ok, M) ->
+    wamp_message:result(M#call.request_id, #{}, [], #{});
 
-maybe_error({ok, Client}, ReqId) ->
-    wamp_message:result(ReqId, #{}, [Client], #{});
+maybe_error({ok, Val}, M) ->
+    wamp_message:result(M#call.request_id, #{}, [Val], #{});
 
-maybe_error({error, Reason}, ReqId) ->
+maybe_error({error, Reason}, M) ->
     #{<<"code">> := Code} = Map = bondy_error:error_map(Reason),
+    Message = case maps:find(<<"message">>, Map) of
+        {ok, Val} -> [Val];
+        error -> undefined
+    end,
     wamp_message:error(
         ?CALL,
-        ReqId,
+        M#call.request_id,
         Map,
-        bondy_error:error_uri(Code)).
+        bondy_error:error_uri(Code),
+        Message
+    );
+
+maybe_error(Val, M) ->
+    wamp_message:result(M#call.request_id, #{}, [Val], #{}).
 
 
 %% @private
-unauthorized(ReqId, Ctxt) ->
-    R = wamp_message:error(
+validate_call_args(Call, Ctxt, MinArity) ->
+    validate_call_args(Call, Ctxt, MinArity, false).
+
+
+%% @private
+validate_admin_call_args(Call, Ctxt, MinArity) ->
+    validate_call_args(Call, Ctxt, MinArity, true).
+
+%% -----------------------------------------------------------------------------
+%% @private
+%% @doc
+%% Validates that the first argument of the call is a RealmUri, defaulting to 
+%% use the session Realm's uri if one is not provided. It uses the MinArity 
+%% to determine wether the RealmUri argument is present or not.
+%% Once the Realm is established it validates it is is equal to the
+%% session's Realm or any other in case the session's realm is the root realm.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec validate_call_args(
+    wamp_call(), 
+    bondy_context:context(), 
+    MinArity :: integer(), 
+    AdminOnly :: boolean()) -> 
+        {ok, Args :: list()} | {error, wamp_error()}.
+        
+validate_call_args(#call{arguments = L} = M, _, N, _) when length(L) + 1 < N ->
+    wamp_message:error(
         ?CALL,
-        ReqId,
+        M#call.request_id,
+        #{},
+        ?WAMP_ERROR_INVALID_ARGUMENT,
+        [<<"At least one argument is missing">>]);
+
+validate_call_args(#call{arguments = []} = M, Ctxt, _, AdminOnly) ->
+    %% We default to the session's Realm
+    case {AdminOnly, bondy_context:realm_uri(Ctxt)} of
+        {false, Uri} ->
+            [Uri];
+        {_, ?BONDY_REALM_URI} ->
+            [?BONDY_REALM_URI];
+        {_, _} ->
+            {error, unauthorized(M)}
+    end;
+
+validate_call_args(#call{arguments = [Uri|_] = L} = M, Ctxt, _, AdminOnly) ->
+    %% A call can only proceed if the session's Realm is the one being
+    %% modified, unless the session's Realm is the Root Realm in which 
+    %% case any Realm can be modified
+    case {AdminOnly, bondy_context:realm_uri(Ctxt)} of
+        {false, Uri} ->
+            L;
+        {_, ?BONDY_REALM_URI} ->
+            L;
+        {_, _} ->
+            {error, unauthorized(M)}
+    end.
+
+
+%% @private
+unauthorized(M) ->
+    wamp_message:error(
+        ?CALL,
+        M#call.request_id,
         #{},
         ?WAMP_ERROR_NOT_AUTHORIZED,
         [
-        iolist:to_binary([
-            <<"The operation you've requested is a targeting a Realm that is not the one you are logged into, or the operation is only supported when you are logged into the Roote Realm (">>, 
-            ?BONDY_REALM_URI, <<$",$)>>
-        ])
-    ]),
-    bondy:send(bondy_context:peer_id(Ctxt), R).
+            iolist:to_binary([
+                <<"The operation you've requested is a targeting a Realm that is not your session's realm or the operation is only supported when you are logged into the Root Realm (">>, 
+                ?BONDY_REALM_URI, <<$",$)>>
+            ])
+    ]).
