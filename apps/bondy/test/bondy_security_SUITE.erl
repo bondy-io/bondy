fac885e656dfb27eeee7648b1f8bd1e7f0af7748
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2017-08-29 16:04:32 +0100

    added more tests, all passing

--- apps/bondy/test/bondy_security_SUITE.erl
+++ apps/bondy/test/bondy_security_SUITE.erl
@@ -21,11 +21,15 @@
 -compile(export_all).
 
 all() ->
-    common:all().
+    [
+        {group, api_client},
+        {group, resource_owner},
+        {group, user}
+    ].
 
 groups() ->
     [
-        {main, [sequence], [
+        {api_client, [sequence], [
             create_realm,
             enable_security,
             security_enabled,
@@ -37,6 +41,22 @@ groups() ->
             api_client_auth2,
             api_client_auth3,
             api_client_delete
+        ]},
+        {resource_owner, [sequence], [
+            resource_owner_add,
+            resource_owner_auth1,
+            resource_owner_update,
+            resource_owner_auth2,
+            resource_owner_auth3,
+            resource_owner_delete
+        ]},
+        {user, [sequence], [
+            user_add,
+            user_auth1,
+            user_update,
+            user_auth2,
+            user_auth3,
+            user_delete
         ]}
     ].
 
@@ -128,4 +148,123 @@ api_client_delete(Config) ->
     {api_client_auth3, Prev} = ?config(saved_config, Config),
     ok = bondy_api_client:remove(
         ?config(realm_uri, Config), ?config(client_id, Prev)),
+    {save_config, Prev}.
+
+resource_owner_add(Config) ->
+    R = #{
+        username => <<"AlE">>,
+        password => <<"ale">>,
+        meta => #{<<"foo">> => <<"bar">>},
+        groups => []
+    },
+    {ok, #{
+        <<"username">> := Username
+    }} = bondy_api_resource_owner:add(?config(realm_uri, Config), R),
+    {save_config, [{username, Username}, {password, <<"ale">>} | Config]}.
+
+resource_owner_auth1(Config) ->
+    {resource_owner_add, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = ?config(username, Prev),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+resource_owner_update(Config) ->
+    {resource_owner_auth1, Prev} = ?config(saved_config, Config),
+    Pass = <<"New-Password">>,
+    {ok, _} = bondy_api_resource_owner:update(
+        ?config(realm_uri, Prev),
+        ?config(username, Prev),
+        #{
+            <<"password">> => Pass,
+            <<"meta">> => #{<<"foo">> => <<"bar">>}
+        }
+    ),
+    {save_config,
+        lists:keyreplace(password, 1, Prev, {password, Pass})}.
+
+resource_owner_auth2(Config) ->
+    {resource_owner_update, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = ?config(username, Prev),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+resource_owner_auth3(Config) ->
+    {resource_owner_auth2, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = unicode_util_compat:uppercase(?config(username, Prev)),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+resource_owner_delete(Config) ->
+    {resource_owner_auth3, Prev} = ?config(saved_config, Config),
+    ok = bondy_api_resource_owner:remove(
+        ?config(realm_uri, Config), ?config(username, Prev)),
+    {save_config, Prev}.
+
+%% USER
+
+
+user_add(Config) ->
+    R = #{
+        username => <<"AlE">>,
+        password => <<"ale">>,
+        meta => #{<<"foo">> => <<"bar">>},
+        groups => []
+    },
+    ok = bondy_security_user:add(?config(realm_uri, Config), R),
+    {save_config, [{username, <<"AlE">>}, {password, <<"ale">>} | Config]}.
+
+user_auth1(Config) ->
+    {user_add, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = ?config(username, Prev),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+user_update(Config) ->
+    {user_auth1, Prev} = ?config(saved_config, Config),
+    Pass = <<"New-Password">>,
+    ok = bondy_security_user:update(
+        ?config(realm_uri, Prev),
+        ?config(username, Prev),
+        #{
+            <<"password">> => Pass,
+            <<"meta">> => #{<<"foo">> => <<"bar">>}
+        }
+    ),
+    {save_config,
+        lists:keyreplace(password, 1, Prev, {password, Pass})}.
+
+user_auth2(Config) ->
+    {user_update, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = ?config(username, Prev),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+user_auth3(Config) ->
+    {user_auth2, Prev} = ?config(saved_config, Config),
+    Uri = ?config(realm_uri, Prev),
+    Username = unicode_util_compat:uppercase(?config(username, Prev)),
+    Pass = ?config(password, Prev),
+    {ok, _} = bondy_security:authenticate(
+        Uri, Username, Pass, [{ip, {127,0,0,1}}]),
+    {save_config, Prev}.
+
+user_delete(Config) ->
+    {user_auth3, Prev} = ?config(saved_config, Config),
+    ok = bondy_security_user:remove(
+        ?config(realm_uri, Config), ?config(username, Prev)),
     {save_config, Prev}.
