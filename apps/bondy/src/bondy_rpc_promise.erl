1cf5716d3f04f17b2d123a8d1d8a4ab7af287534
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-10-15 11:52:56 +0100

    log change

--- apps/bondy/src/bondy_rpc_promise.erl
+++ apps/bondy/src/bondy_rpc_promise.erl
@@ -171,7 +171,7 @@ enqueue(RealmUri, #bondy_rpc_promise{} = P, Timeout) ->
         _ = lager:debug(
             "RPC Promise evicted from queue;"
             " realm_uri=~p, caller_session_id=~p, invocation_id=~p, call_id=~p"
-            " ttl=~p",
+            " timeout=~p",
             [RealmUri, element(3, Key), InvocationId, CallId, Timeout]
         )
     end,
