984778df2b493bd58606740f71b45ae0181e928d
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-11-21 17:21:59 +0000

    final consistent hashing algo
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_consistent_hashing.erl
+++ apps/bondy/src/bondy_consistent_hashing.erl
@@ -1,3 +1,21 @@
+%% =============================================================================
+%%  babel_consistent_hashing.erl -
+%%  Copyright (c) 2016-2019 Ngineo Limited. All rights reserved.
+%%  Copyright (c) 2020 Leapsight Holdings Limited. All rights reserved.
+%%
+%%  Licensed under the Apache License, Version 2.0 (the "License");
+%%  you may not use this file except in compliance with the License.
+%%  You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%  Unless required by applicable law or agreed to in writing, software
+%%  distributed under the License is distributed on an "AS IS" BASIS,
+%%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%  See the License for the specific language governing permissions and
+%%  limitations under the License.
+%% =============================================================================
+
 %% -----------------------------------------------------------------------------
 %% @doc
 %% It uses Jump Consistent Hash algorithm described in
@@ -43,8 +61,7 @@ bucket(_, 1, _) ->
 
 bucket(Key, Buckets, jch)
 when is_integer(Key) andalso is_integer(Buckets) andalso Buckets > 1 ->
-    State = rand:seed_s(exs1024s, {Key, Key, Key}),
-    jch(-1, 0, Buckets, State);
+    jump_consistent_hash(Key, Buckets);
 
 bucket(Key, Buckets, Algo) ->
     bucket(erlang:phash2(Key), Buckets, Algo).
@@ -57,11 +74,59 @@ bucket(Key, Buckets, Algo) ->
 
 
 
-%% @private
-jch(Bucket, Jump, Buckets, _) when Jump >= Buckets ->
-    Bucket;
+jump_consistent_hash(Key, N) ->
+    jump_consistent_hash(Key, N, -1, 0).
 
-jch(_, Jump, Buckets, State0) ->
-    {Random, State1} = rand:uniform_s(State0),
-    NewJump = trunc((Jump + 1) / Random),
-    jch(Jump, NewJump, Buckets, State1).
+
+%% -----------------------------------------------------------------------------
+%% @private
+%% @doc
+%% The following is the C++ implementation in
+%% A Fast, Minimal Memory, Consistent Hash Algorithm
+%% https://arxiv.org/pdf/1406.2294.pdf
+%%
+%% static int32_t jump_consistent_hash(uint64_t key, int32_t num_buckets) {
+%%   int64_t b = -1, j = 0;
+%%   while (j < num_buckets) {
+%%     b = j;
+%%     key = key * 2862933555777941757ULL + 1;
+%%     j = (b + 1) * ((double)(1LL << 31) / (double)((key >> 33) + 1));
+%%   }
+%%   return (int32_t)b;
+%% }
+%% @end
+%% -----------------------------------------------------------------------------
+
+jump_consistent_hash(Key, N, _, J0) when J0 < N ->
+    %% B1 = J0,
+    NewKey = (Key * ?MAGIC + 1) band ?MASK,
+    J1 = trunc((J0 + 1) * ((1 bsl 31) / ((NewKey bsr 33) + 1)) ),
+    jump_consistent_hash(NewKey, N, J0, J1);
+
+jump_consistent_hash(_, _, B, _) ->
+    B.
+
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+jch_test_() ->
+    Cases =
+        %% {Expect, Key, Buckets}
+        [
+            {0, 0, 1},
+            {0, 3, 1},
+            {0, 0, 2},
+            {1, 4, 2},
+            {0, 7, 2},
+            {55, 1, 128},
+            {120, 129, 128},
+            {0, 0, 100000000},
+            {38172097, 128, 100000000},
+            {1644467860, 128, 2147483648},
+            {92, 18446744073709551615, 128}
+        ],
+    [?_assertEqual(Expect, bucket(K, B, jch)) || {Expect, K, B} <- Cases].
+
+-endif.
