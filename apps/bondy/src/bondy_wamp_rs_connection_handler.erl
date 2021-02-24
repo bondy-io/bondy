3709bca5e590bf0c0fac0b5d828544c0d97bd060
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-02-24 14:37:12 +0000

    renamed option
    wamp.tcp.serializers to wamp.rawsocket.serializer -> wamp_rawsocket
    
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_wamp_rs_connection_handler.erl
+++ apps/bondy/src/bondy_wamp_rs_connection_handler.erl
@@ -619,7 +619,7 @@ validate_encoding(2) ->
     {binary, msgpack};
 
 validate_encoding(N) ->
-    case bondy_config:get([wamp_tcp, serializers, N], undefined) of
+    case bondy_config:get([wamp_rawsocket, serializers, N], undefined) of
         erl ->
             {binary, erl};
         bert ->
