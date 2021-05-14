46331e666f3955646276b1a13a30f346c61dd6af
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-05-14 07:01:55 +0100

    added more auth tests
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/test/bondy_auth_password_SUITE.erl
+++ apps/bondy/test/bondy_auth_password_SUITE.erl
@@ -20,19 +20,167 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
 
+-include("bondy_security.hrl").
+
+-define(U1, <<"user_1">>).
+-define(U2, <<"user_2">>).
+-define(P1, <<"aWe11KeptSecret">>).
+-define(P2, <<"An0therWe11KeptSecret">>).
 
 -compile([nowarn_export_all, export_all]).
 
 all() ->
     [
-
+        test_1
     ].
 
 
 init_per_suite(Config) ->
     common:start_bondy(),
-    [{realm_uri, <<"com.myrealm">>}|Config].
+    RealmUri = <<"com.example.test.auth_password">>,
+    ok = add_realm(RealmUri),
+    [{realm_uri, RealmUri}|Config].
 
 end_per_suite(Config) ->
     % common:stop_bondy(),
     {save_config, Config}.
+
+
+add_realm(RealmUri) ->
+    Config = #{
+        uri => RealmUri,
+        description => <<"A test realm">>,
+        authmethods => [
+            ?PASSWORD_AUTH
+        ],
+        security_enabled => true,
+        grants => [
+            #{
+                permissions => [
+                    <<"wamp.register">>,
+                    <<"wamp.unregister">>,
+                    <<"wamp.subscribe">>,
+                    <<"wamp.unsubscribe">>,
+                    <<"wamp.call">>,
+                    <<"wamp.cancel">>,
+                    <<"wamp.publish">>
+                ],
+                uri => <<"">>,
+                match => <<"prefix">>,
+                roles => <<"all">>
+            }
+        ],
+        users => [
+            #{
+                username => ?U1,
+                password => ?P1,
+                groups => [],
+                meta => #{}
+            },
+            #{
+                username => ?U2,
+                password => ?P1,
+                groups => [],
+                meta => #{}
+            }
+        ],
+        sources => [
+            #{
+                usernames => [?U1],
+                authmethod => ?PASSWORD_AUTH,
+                cidr => <<"0.0.0.0/0">>
+            },
+            #{
+                usernames => [?U2],
+                authmethod => ?PASSWORD_AUTH,
+                cidr => <<"198.162.0.0/16">>
+            }
+        ]
+    },
+    _ = bondy_realm:add(Config),
+    ok.
+
+
+test_1(Config) ->
+    RealmUri = ?config(realm_uri, Config),
+    SessionId = 1,
+    Roles = [],
+    Peer = {{127, 0, 0, 1}, 10000},
+
+    Ctxt1 = bondy_auth:init(SessionId, RealmUri, ?U1, Roles, Peer),
+
+    ?assertEqual(
+        true,
+        lists:member(?PASSWORD_AUTH, bondy_auth:available_methods(Ctxt1))
+    ),
+
+    ?assertMatch(
+        {ok, _, _},
+        bondy_auth:authenticate(?PASSWORD_AUTH, ?P1, undefined, Ctxt1)
+    ),
+
+    ?assertMatch(
+        {error, method_not_allowed},
+        bondy_auth:authenticate(?WAMP_CRA_AUTH, ?P1, undefined, Ctxt1)
+    ),
+
+    ?assertMatch(
+        {error, invalid_method},
+        bondy_auth:authenticate(<<"foo">>, ?P1, undefined, Ctxt1)
+    ),
+
+    %% user 2 is not granted access from Peer (see test_2)
+    Ctxt2 = bondy_auth:init(SessionId, RealmUri, ?U2, Roles, Peer),
+
+    ?assertEqual(
+        false,
+        lists:member(?PASSWORD_AUTH, bondy_auth:available_methods(Ctxt2))
+    ),
+
+    ?assertMatch(
+        {error, method_not_allowed},
+        bondy_auth:authenticate(?PASSWORD_AUTH, ?P1, undefined, Ctxt2)
+    ).
+
+
+
+test_2(Config) ->
+    RealmUri = ?config(realm_uri, Config),
+    SessionId = 1,
+    Roles = [],
+    Peer = {{192, 168, 0, 1}, 10000},
+
+    Ctxt2 = bondy_auth:init(SessionId, RealmUri, ?U2, Roles, Peer),
+
+    ?assertEqual(
+        true,
+        lists:member(?PASSWORD_AUTH, bondy_auth:available_methods(Ctxt2))
+    ),
+
+    ?assertMatch(
+        {ok, _, _},
+        bondy_auth:authenticate(?PASSWORD_AUTH, ?P1, undefined, Ctxt2)
+    ),
+
+    ?assertMatch(
+        {error, method_not_allowed},
+        bondy_auth:authenticate(?WAMP_CRA_AUTH, ?P1, undefined, Ctxt2)
+    ),
+
+    ?assertMatch(
+        {error, invalid_method},
+        bondy_auth:authenticate(<<"foo">>, ?P1, undefined, Ctxt2)
+    ),
+
+    %% user 1 is not granted access from Peer (see test_1)
+    Ctxt1 = bondy_auth:init(SessionId, RealmUri, ?U1, Roles, Peer),
+
+    ?assertEqual(
+        false,
+        lists:member(?PASSWORD_AUTH, bondy_auth:available_methods(Ctxt1))
+    ),
+
+    ?assertMatch(
+        {error, method_not_allowed},
+        bondy_auth:authenticate(?PASSWORD_AUTH, ?P1, undefined, Ctxt1)
+    ).
