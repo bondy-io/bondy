c1d1593fdd8a758a6c39fe0878e3443d80c533de
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-05-17 15:56:53 +0100

    cryptosign fixes
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_auth_wamp_cryptosign.erl
+++ apps/bondy/src/bondy_auth_wamp_cryptosign.erl
@@ -100,18 +100,22 @@ challenge(Details, Ctxt, State) ->
         ),
         Key =/= undefined orelse throw(missing_pubkey),
 
+        %% The stored keys are hex formatted so that we can easily compare here
         Keys = bondy_rbac_user:authorized_keys(bondy_auth:user(Ctxt)),
 
         case lists:member(Key, Keys) of
             true ->
-                Message = enacl:randombytes(32),
-                Extra = #{
-                    challenge => hex_utils:bin_to_hexstr(Message),
-                    channel_binding => undefined %% TODO
-                },
+                % Message = enacl:randombytes(32),
+                Message = <<187,172,178,156,100,104,78,221,32,249,69,117,86,248,250,66,113,22,86,98,229,238,44,121,191,227,190,84,187,131,183,38>>,
+
                 NewState = State#{
-                    pubkey => Key,
-                    expected_message => Message
+                    pubkey => decode_hex(Key),
+                    message => Message
+                },
+
+                Extra = #{
+                    challenge => encode_hex(Message),
+                    channel_binding => undefined %% TODO
                 },
                 {ok, Extra, NewState};
             false ->
@@ -135,22 +139,19 @@ challenge(Details, Ctxt, State) ->
     {ok, DataOut :: map(), CBState :: state()}
     | {error, Reason :: any(), CBState :: state()}.
 
-authenticate(EncMessage, _, _, #{pubkey := PK} = State)
-when is_binary(EncMessage) ->
+authenticate(EncSignature, _, _, #{pubkey := PK} = State)
+when is_binary(EncSignature) ->
     try
-        SignedMessage = decode_hex(EncMessage),
-        ExpectedMessage = maps:get(expected_message, State),
+        Signature = decode_hex(EncSignature),
+        Message = maps:get(message, State),
 
         %% Verify that the message was signed using the Ed25519 key
-        case enacl:sign_open(SignedMessage, PK) of
-            {ok, ExpectedMessage} ->
+        case enacl:sign_verify_detached(Signature, Message, PK) of
+            true ->
                 {ok, #{}, State};
 
-            {ok, _} ->
+            false ->
                 %% Message does not match the expected
-                {error, authentication_failed, State};
-
-            {error, failed_verification} ->
                 {error, invalid_signature, State}
         end
     catch
@@ -170,11 +171,12 @@ when is_binary(EncMessage) ->
 
 %% @private
 decode_hex(HexString) ->
-    try hex_utils:hexstr_to_bin(HexString) of
-        Bin when byte_size(Bin) == 96 ->
-            Bin;
-        _ ->
-            throw(invalid_siognature_length)
+    try hex_utils:hexstr_to_bin(HexString)
+    % of
+    %     Bin when byte_size(Bin) == 96 ->
+    %         Bin;
+    %     _ ->
+    %         throw(invalid_signature_length)
     catch
         throw:Reason ->
             throw(Reason);
@@ -183,6 +185,9 @@ decode_hex(HexString) ->
     end.
 
 
+%% @private
+encode_hex(Bin) when is_binary(Bin) ->
+    list_to_binary(hex_utils:bin_to_hexstr(Bin)).
 
 
 
