14b3e7d5f5e46cb9cff920162ed72d61906e71b4
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-07-23 20:14:58 +0100

    fixed adding a user in a realm and SSO when it already exists in SSO realm
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/test/bondy_rbac_user_SUITE.erl
+++ apps/bondy/test/bondy_rbac_user_SUITE.erl
@@ -25,7 +25,8 @@
 -define(LU2, <<"local_user_2">>).
 -define(SSOU1, <<"sso_user_1">>).
 -define(SSOU2, <<"sso_user_2">>).
--define(REALM_URI, <<"com.example.test.user">>).
+-define(REALM1_URI, <<"com.example.test1">>).
+-define(REALM2_URI, <<"com.example.test2">>).
 -define(SSO_REALM_URI, <<"com.example.test.user.sso">>).
 
 -compile([nowarn_export_all, export_all]).
@@ -34,24 +35,62 @@ all() ->
     [
         test,
         invalid_sso_realm,
-        resolve
+        resolve,
+        add_sso_user_to_realm
     ].
 
 
 init_per_suite(Config) ->
     common:start_bondy(),
     KeyPairs = [enacl:crypto_sign_ed25519_keypair() || _ <- lists:seq(1, 3)],
-    RealmUri = ?REALM_URI,
+    PubKeys = [
+        maps:get(public, KeyPair)
+        || KeyPair <- KeyPairs
+    ],
     SSORealmUri = ?SSO_REALM_URI,
     ok = add_sso_realm(SSORealmUri),
-    ok = add_realm(RealmUri, SSORealmUri, KeyPairs),
-    [{realm_uri, RealmUri}, {keypairs, KeyPairs} | Config].
+    ok = add_realm(?REALM1_URI, SSORealmUri, KeyPairs, [
+        #{
+            username => ?LU1,
+            authorized_keys => PubKeys,
+            groups => [],
+            meta => #{fruit => <<"apple">>}
+        },
+        #{
+            username => ?LU2,
+            password => ?LU2,
+            groups => [],
+            meta => #{fruit => <<"banana">>}
+        },
+        #{
+            username => ?SSOU1,
+            authorized_keys => PubKeys,
+            groups => [],
+            meta => #{fruit => <<"passion fruit">>},
+            sso_opts => #{
+                realm_uri => ?SSO_REALM_URI,
+                groups => [],
+                meta => #{fruit => <<"mango">>}
+            }
+        },
+        #{
+            username => ?SSOU2,
+            password => ?SSOU2,
+            groups => [],
+            meta => #{fruit => <<"orange">>},
+            sso_opts => #{
+                realm_uri => ?SSO_REALM_URI,
+                groups => [],
+                meta => #{fruit => <<"grapefruit">>}
+            }
+        }
+    ]),
+    ok = add_realm(?REALM2_URI, SSORealmUri, KeyPairs, []),
+    [{keypairs, KeyPairs} | Config].
 
 end_per_suite(Config) ->
     % common:stop_bondy(),
     {save_config, Config}.
-
-
 
 
 add_sso_realm(RealmUri) ->
@@ -67,11 +106,7 @@ add_sso_realm(RealmUri) ->
     ok.
 
 
-add_realm(RealmUri, SSORealmUri, KeyPairs) ->
-    PubKeys = [
-        maps:get(public, KeyPair)
-        || KeyPair <- KeyPairs
-    ],
+add_realm(RealmUri, SSORealmUri, _KeyPairs, Users) ->
 
     Config = #{
         uri => RealmUri,
@@ -109,60 +144,27 @@ add_realm(RealmUri, SSORealmUri, KeyPairs) ->
                 cidr => <<"0.0.0.0/0">>
             }
         ],
-        users => [
-            #{
-                username => ?LU1,
-                authorized_keys => PubKeys,
-                groups => [],
-                meta => #{fruit => <<"apple">>}
-            },
-            #{
-                username => ?LU2,
-                password => ?LU2,
-                groups => [],
-                meta => #{fruit => <<"banana">>}
-            },
-            #{
-                username => ?SSOU1,
-                authorized_keys => PubKeys,
-                groups => [],
-                meta => #{fruit => <<"passion fruit">>},
-                sso_opts => #{
-                    realm_uri => ?SSO_REALM_URI,
-                    groups => [],
-                    meta => #{fruit => <<"mango">>}
-                }
-            },
-            #{
-                username => ?SSOU2,
-                password => ?SSOU2,
-                groups => [],
-                meta => #{fruit => <<"orange">>},
-                sso_opts => #{
-                    realm_uri => ?SSO_REALM_URI,
-                    groups => [],
-                    meta => #{fruit => <<"grapefruit">>}
-                }
-            }
-
-        ]
+        users => Users
     },
     _ = bondy_realm:add(Config),
     ok.
 
 
+
 test(_) ->
-    _LU1 = bondy_rbac_user:fetch(?REALM_URI, ?LU1),
-    _LU2 = bondy_rbac_user:fetch(?REALM_URI, ?LU2),
-    _ = bondy_rbac_user:fetch(?REALM_URI, ?SSOU1),
-    _ = bondy_rbac_user:fetch(?REALM_URI, ?SSOU2),
+    _LU1 = bondy_rbac_user:fetch(?REALM1_URI, ?LU1),
+    _LU2 = bondy_rbac_user:fetch(?REALM1_URI, ?LU2),
+    _ = bondy_rbac_user:fetch(?REALM1_URI, ?SSOU1),
+    _ = bondy_rbac_user:fetch(?REALM1_URI, ?SSOU2),
     _SSOU1 = bondy_rbac_user:fetch(?SSO_REALM_URI, ?SSOU1),
     _SSOU2 = bondy_rbac_user:fetch(?SSO_REALM_URI, ?SSOU2),
     ok.
 
 
+
+
 resolve(_) ->
-    Local = bondy_rbac_user:fetch(?REALM_URI, ?SSOU1),
+    Local = bondy_rbac_user:fetch(?REALM1_URI, ?SSOU1),
     Resolved = bondy_rbac_user:resolve(Local),
 
     ?assertEqual(
@@ -203,13 +205,48 @@ invalid_sso_realm(Config) ->
     },
     ?assertEqual(
         {error, role_exists},
-        bondy_rbac_user:add(?REALM_URI, bondy_rbac_user:new(User0))
+        bondy_rbac_user:add(?REALM1_URI, bondy_rbac_user:new(User0))
     ),
 
     User1 = User0#{username => <<"foo">>},
     ?assertEqual(
         {error, invalid_sso_realm},
-        bondy_rbac_user:add(?REALM_URI, bondy_rbac_user:new(User1))
+        bondy_rbac_user:add(?REALM1_URI, bondy_rbac_user:new(User1))
     ).
 
 
+add_sso_user_to_realm(_) ->
+    SSOU1 = bondy_rbac_user:fetch(?SSO_REALM_URI, ?SSOU1),
+    User0 = #{
+        username => ?SSOU1,
+        password => <<"thisWillBeDroped">>,
+        groups => [],
+        meta => #{fruit => <<"passion fruit">>},
+        sso_opts => #{
+            realm_uri => ?SSO_REALM_URI,
+            groups => [],
+            meta => #{fruit => <<"not mango">>}
+        }
+    },
+
+    ?assertEqual(
+        {error, not_found},
+        bondy_rbac_user:lookup(?REALM2_URI, ?SSOU1)
+    ),
+
+    {ok, NewUser} = bondy_rbac_user:add(
+        ?REALM2_URI, bondy_rbac_user:new(User0)
+    ),
+
+    %% Because we are adding an existing SSO user to a new Realm the password
+    %% is discarded
+    ?assertEqual(
+        false,
+        bondy_rbac_user:has_password(NewUser)
+    ),
+
+    %% And the SSO groups, meta were for the SSO user were also discarded
+    ?assertEqual(
+        SSOU1,
+        bondy_rbac_user:fetch(?SSO_REALM_URI, ?SSOU1)
+    ).
