7d38f8ab4470341c1c6eea8641df11c8d2f182c9
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-08-10 11:54:59 +0100

    improve API
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_wamp_meta_api.erl
+++ apps/bondy/src/bondy_wamp_meta_api.erl
@@ -64,22 +64,18 @@ handle_call(M, Ctxt) ->
 %% =============================================================================
 
 
+
 do_handle(#call{procedure_uri = ?WAMP_SESSION_GET} = M, Ctxt) ->
     [RealmUri, SessionId] = bondy_wamp_utils:validate_call_args(M, Ctxt, 2),
-    case bondy_session:lookup(SessionId) of
+    case bondy_session:lookup(RealmUri, SessionId) of
         {error, not_found} ->
-            {error, bondy_wamp_utils:no_such_session_error(SessionId)};
+            bondy_wamp_utils:no_such_session_error(SessionId);
         Session ->
-            case bondy_session:realm_uri(Session) of
-                RealmUri ->
-                    wamp_message:result(
-                        M#call.request_id,
-                        #{},
-                        [bondy_session:to_external(Session)]
-                    );
-                _ ->
-                    {error, bondy_wamp_utils:no_such_session_error(SessionId)}
-            end
+            wamp_message:result(
+                M#call.request_id,
+                #{},
+                [bondy_session:to_external(Session)]
+            )
     end;
 
 
