18b22437816ae6682f434e67c9e4cd92448efcad
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2021-12-02 15:25:15 +0000

    changes to egde sup
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_edge_exchanges_sup.erl
+++ apps/bondy/src/bondy_edge_exchanges_sup.erl
@@ -1,5 +1,5 @@
 %% =============================================================================
-%%  bondy_edge_uplink_client_sup.erl -
+%%  bondy_edge_uplink_session_sup.erl -
 %%
 %%  Copyright (c) 2018-2021 Leapsight. All rights reserved.
 %%
@@ -20,18 +20,11 @@
 %% @doc
 %% @end
 %% -----------------------------------------------------------------------------
--module(bondy_edge_uplink_client_sup).
+-module(bondy_edge_exchanges_sup).
 
 -behaviour(supervisor).
 
--define(SUPERVISOR(Id, Args, Restart, Timeout), #{
-    id => Id,
-    start => {Id, start_link, Args},
-    restart => Restart,
-    shutdown => Timeout,
-    type => supervisor,
-    modules => [Id]
-}).
+-include_lib("wamp/include/wamp.hrl").
 
 -define(WORKER(Id, Args, Restart, Timeout), #{
     id => Id,
@@ -44,6 +37,8 @@
 
 %% API
 -export([start_link/0]).
+-export([start_exchange/3]).
+-export([stop_exchange/1]).
 
 
 %% SUPERVISOR CALLBACKS
@@ -65,6 +60,36 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 
+%% -----------------------------------------------------------------------------
+%% @doc Starts a new exchange provided we would not reach the limit set by the
+%% `aae_concurrency' config parameter.
+%% If the limit is reached returns the error tuple `{error, concurrency_limit}'
+%% @end
+%% -----------------------------------------------------------------------------
+-spec start_exchange(
+    Conn :: pid(), Sessions :: [bondy_edge_session:t()], Opts :: map()) ->
+    {ok, pid()} | {error, any()}.
+
+start_exchange(Conn, Sessions, Opts) ->
+    Children = supervisor:count_children(?MODULE),
+    {active, Count} = lists:keyfind(active, 1, Children),
+    case bondy_config:get([edge, aae_concurrency], 1) > Count of
+        true ->
+            Args = [Conn, Sessions, Opts],
+            supervisor:start_child(?MODULE, Args);
+        false ->
+            {error, concurrency_limit}
+    end.
+
+
+%% -----------------------------------------------------------------------------
+%% @doc
+%% @end
+%% -----------------------------------------------------------------------------
+stop_exchange(Pid) when is_pid(Pid)->
+    supervisor:terminate_child(?MODULE, Pid).
+
+
 
 %% =============================================================================
 %% SUPERVISOR CALLBACKS
@@ -73,27 +98,9 @@ start_link() ->
 
 
 init([]) ->
-    Opts = bondy_config:get([edge, uplink]),
-
-    case key_value:get(enabled, Opts) of
-        true ->
-            Args = [
-                key_value:get(transport, Opts),
-                key_value:get(endpoint, Opts),
-                Opts
-            ],
-
-            Children = [
-                ?WORKER(
-                    bondy_edge_uplink_client, Args, permanent, 5000
-                ),
-                ?SUPERVISOR(
-                    bondy_edge_exchanges_sup, Args, permanent, infinity
-                )
-            ],
-            {ok, {{rest_for_one, 5, 60}, Children}};
-
-        false ->
-            ignore
-    end.
+    Children = [
+        ?WORKER(bondy_edge_exchange_statem, [], temporary, 5000)
+    ],
+    Specs = {{simple_one_for_one, 0, 1}, Children},
+    {ok, Specs}.
 
