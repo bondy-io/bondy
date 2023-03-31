e72460e094cd0d897e74a5c2864c6eaa3cd7a213
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-03-31 15:04:56 +0100

    update copyright
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- apps/bondy/src/bondy_relay.erl
+++ apps/bondy/src/bondy_relay.erl
@@ -3,7 +3,7 @@
 %%  ERROR), INTERRUPT and PUBLISH messages between WAMP clients connected to
 %%  different Bondy peers (nodes).
 %%
-%%  Copyright (c) 2016-2022 Leapsight. All rights reserved.
+%%  Copyright (c) 2016-2023 Leapsight. All rights reserved.
 %%
 %%  Licensed under the Apache License, Version 2.0 (the "License");
 %%  you may not use this file except in compliance with the License.
