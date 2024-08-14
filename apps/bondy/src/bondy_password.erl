d3660250728aeb5bfa8060e7d437506099d6db80
Alejandro Miguez <alejandro.miguez@leapsight.com>
2024-08-14 12:19:00 -0300

    Fixes but with questions related to some errors collected from one deployment with the latest version 1.0.0-rc.22

--- apps/bondy/src/bondy_password.erl
+++ apps/bondy/src/bondy_password.erl
@@ -399,6 +399,11 @@ verify_string(String, #{version := <<"1.0">>} = PW) ->
     %% StoredHash is hex value
     pbkdf2:compare_secure(pbkdf2:to_hex(Hash), StoredHash);
 
+%% to handle the error: reason=function_clause
+%% example: [{bondy_password,verify_string,[<<\"Nes 2907\">>,[{hash_pass,<<\"adcebee9a2cbbe4e26c340f95da646a1ab60c676\">>},{auth_name,pbkdf2},{hash_func,sha},{salt,<<76,202,0,27,196,167,217,222,194,142,96,185,219,169,96,233>>},{iterations,65536}]]
+verify_string(Hash, PWList) when is_list(PWList) ->
+    verify_string(Hash, maps:from_list(PWList));
+
 verify_string(Hash, #{} = PW) ->
     verify_string(Hash, add_version(PW)).
 
